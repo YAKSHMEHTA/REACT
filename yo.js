00ef48a188f7f7dccd087c870f145fad6d75e77a
YAKSHMEHTA <yakshvardhansinghmehta@gamil.com>
2025-11-01 11:12:47 +0530

    Custom hook

---
+++ yo.js
@@ -0,0 +1,13 @@
+async function convertCurrency(from, to, amount) {
+  try {
+    const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const rate = data[from][to];
+    console.log(`${amount} ${from.toUpperCase()} = ${(amount * rate).toFixed(2)} ${to.toUpperCase()}`);
+  } catch (err) {
+    console.error("Error:", err.message);
+  }
+}
+ 
+convertCurrency("usd", "inr", 10);
