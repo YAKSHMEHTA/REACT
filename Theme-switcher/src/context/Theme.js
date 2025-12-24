070bb9b73bc69e83361028b66459c84e21e3fafd
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2025-12-24 21:14:00 +0530

    Context learnt

---
+++ Theme-switcher/src/context/Theme.js
@@ -0,0 +1,15 @@
+import React,{createContext,useContext} from 'react'
+
+
+
+export  const ThemeContext = createContext({
+  themeMode:"light",
+  lightTheme: ()=>{},
+  blackTheme: ()=>{},
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+  return useContext(ThemeContext)
+}
