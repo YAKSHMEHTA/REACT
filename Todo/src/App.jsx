0538d7260053cb8f3f2215d2894ef1a1cd15d663
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2025-12-25 20:29:00 +0530

    working on it

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -4,10 +4,17 @@ import { TodoProvider } from "./context/ToContex";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const addTodo = (todo) =>{
+    setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+  }
+
+  const updateTodo = (id,todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo :prevTodo)))
+  }
 
   return (
     <>
-      <TodoProvider>
+      <TodoProvider value={{todos,addTodo,updateTodo,delTodo,updateTodo}} >
         <div className="bg-[#172842] min-h-screen py-8">
           <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">
