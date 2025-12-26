53aed14967c410ab55ba51198c41053d217cf08a
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2025-12-26 17:04:32 +0530

    local storage

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css";
 import { TodoProvider } from "./context/ToContex";
 
@@ -19,6 +19,17 @@ function App() {
   const toggleComp = (id) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? "true" : prevTodo))
   }
+
+  useEffect(()=> {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  },[])
+  useEffect(()=> {
+    localStorage.setItem("todos",JSON.stringify())
+    
+  },[todos])
 
   return (
     <>
